dacf4cd66dcdacf625cef5a47b57387e2760f959
Hilal-Ahmad786 <hilalahmad.civilengineer@gmail.com>
2026-04-23 20:42:00 +0300

    jfdlsjfl

--- src/app/(main)/booking/payment/page.tsx
+++ src/app/(main)/booking/payment/page.tsx
@@ -252,9 +252,8 @@ function PaymentContent() {
                     options={{
                       clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID!,
                       currency: 'EUR',
-                      intent: 'capture',
                       components: 'buttons',
-                      enableFunding: 'card',
+                      locale: 'en_US',
                     }}
                   >
                     <PayPalButtonsWrapper
